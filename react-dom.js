5c867388b6ccfcea4808d706d5bbb33a715e4e77
Crazy Urus <crazyurus@vip.qq.com>
2022-05-17 12:03:43 +0800

    feat: fiber

--- react-dom.js
+++ react-dom.js
@@ -1,18 +1,80 @@
 const excludeProps = ['children'];
 
-export function render(element, container) {
-  const { type, props } = element;
+function createDOM(fiber) {
+  const { type, props } = fiber;
   const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
-
-  if (props.children) {
-    props.children.forEach(child => {
-      render(child, dom);
-    });
-  }
 
   Object.keys(props).filter(key => !key.includes(excludeProps)).forEach(name => {
     dom[name] = props[name];
   });
 
-  container.appendChild(dom);
+  return dom;
 }
+
+let nextUnitOfWork = null;
+
+export function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+
+  requestIdleCallback(workLoop);
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children || [];
+  let prevSibling = null;
+
+  elements.forEach((element, index) => {
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+  });
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
+}
+
